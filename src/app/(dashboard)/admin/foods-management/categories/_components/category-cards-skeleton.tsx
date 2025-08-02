0350274e116b48c7540dfc806b1dfb08bec2e152
Jorge Matos <jisapjisap@gmail.com>
2025-08-02 16:53:29 +0200

    Integrate loading skeleton into CategoryCards component to enhance user experience during data fetching; display CategoryCardsSkeleton when categories are loading.

--- src/app/(dashboard)/admin/foods-management/categories/_components/category-cards-skeleton.tsx
+++ src/app/(dashboard)/admin/foods-management/categories/_components/category-cards-skeleton.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { Skeleton } from "@/components/ui/skeleton";
 
 const CategoryCardsSkeleton = () => {
--- src/app/(dashboard)/admin/foods-management/categories/_components/category-cards.tsx
+++ src/app/(dashboard)/admin/foods-management/categories/_components/category-cards.tsx
@@ -7,6 +7,7 @@ import { Button } from '@/components/ui/button';
 import { Edit, Trash } from 'lucide-react';
 import { alert } from '@/lib/use-global-store';
 import { useCategoriesStore } from '../_libs/useCategoriesStore';
+import { CategoryCardsSkeleton } from './category-cards-skeleton';
 
 
 const CategoryCards = () => {
@@ -20,41 +21,47 @@ const CategoryCards = () => {
 
   return (
     <div className='grid grid-cols-4 gap-2'>
-      {categoriesQuery.data?.map((item) => (
-        <div key={item.id} className='bg-accent flex flex-col justify-between gap-3 rounded-lg p-6 shadow-md'>
-          <p className='truncate'>
-            {item.name}
-          </p>
-          <div className='flex gap-1'>
-            <Button 
-              className='size-6' 
-              variant="ghost" 
-              size="icon" 
-              onClick={() => {
-                updateSelectedCategoryId(item.id);  // Establece el ID de la categoría seleccionada
-                updateCategoryDialogOpen(true);     // Abre el dialogo de edición de categoría <CategoryFormDialog /> -> permite cambiar el nombre de la categoría
-              }}
-            >
-              <Edit />
-            </Button>
-            <Button 
-              className='size-6' 
-              variant="ghost" 
-              size="icon" 
-              onClick={() => {
-                alert({
-                  title: "Delete Category",
-                  description: "Are you sure you want to delete this category?",
-                  confirmLabel: "Delete",
-                  cancelLabel: "Cancel",
-                  onConfirm: () => deleteCategoryMutation.mutate(item.id),
-                })
-              }}>
-              <Trash />
-            </Button>
-          </div>
-        </div>
-      ))}
+      {categoriesQuery.isLoading ? (
+        <CategoryCardsSkeleton />
+      ) : (
+        <>
+          {categoriesQuery.data?.map((item) => (
+            <div key={item.id} className='bg-accent flex flex-col justify-between gap-3 rounded-lg p-6 shadow-md'>
+              <p className='truncate'>
+                {item.name}
+              </p>
+              <div className='flex gap-1'>
+                <Button 
+                  className='size-6' 
+                  variant="ghost" 
+                  size="icon" 
+                  onClick={() => {
+                    updateSelectedCategoryId(item.id);  // Establece el ID de la categoría seleccionada
+                    updateCategoryDialogOpen(true);     // Abre el dialogo de edición de categoría <CategoryFormDialog /> -> permite cambiar el nombre de la categoría
+                  }}
+                >
+                  <Edit />
+                </Button>
+                <Button 
+                  className='size-6' 
+                  variant="ghost" 
+                  size="icon" 
+                  onClick={() => {
+                    alert({
+                      title: "Delete Category",
+                      description: "Are you sure you want to delete this category?",
+                      confirmLabel: "Delete",
+                      cancelLabel: "Cancel",
+                      onConfirm: () => deleteCategoryMutation.mutate(item.id),
+                    })
+                  }}>
+                  <Trash />
+                </Button>
+              </div>
+            </div>
+          ))}
+        </>
+      )}
     </div>
   )
 }
