aaefa084d38201ced9d8f065622224ff4ffbf253
Jorge Matos <jisapjisap@gmail.com>
2025-08-09 15:19:09 +0200

    Add MealFilters component for filtering meals by date. Integrate form handling with Zod validation and connect to the meal store for updating filters. This enhances user interaction in the dashboard by allowing date-based meal filtering.

---
+++ src/app/(dashboard)/client/_components/mealFilters.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { useMealsStore } from "@/app/(dashboard)/client/_libs/use-meal-store";
+import {
+  mealFiltersDefaultValues,
+  mealFiltersSchema,
+  MealFiltersSchema,
+} from "@/app/(dashboard)/client/_types/mealFilterSchema";
+import { Button } from "@/components/ui/button";
+import { ControlledDatePicker } from "@/components/ui/controlled/controlled-date-picker";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
+import z from "zod";
+
+const MealFilters = () => {
+  
+  const form = useForm<z.input<typeof mealFiltersSchema>>({
+    defaultValues: mealFiltersDefaultValues,
+    resolver: zodResolver(mealFiltersSchema),
+  });
+
+  const { updateMealFilters } = useMealsStore();
+
+  const onSubmit: SubmitHandler<MealFiltersSchema> = (data) => {
+    updateMealFilters(data);
+  };
+
+  return (
+    <FormProvider {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit as any)}
+        className="mb-4 flex items-center gap-3"
+      >
+        <ControlledDatePicker<z.input<typeof mealFiltersSchema>>
+          name="dateTime"
+          label="Filter by date"
+        />
+        <Button type="submit" size="sm">
+          Apply
+        </Button>
+      </form>
+    </FormProvider>
+  );
+};
+
+export { MealFilters };
